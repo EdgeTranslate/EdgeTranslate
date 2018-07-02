6fa2f1389f9227d71607c4f613279599bb1566f7
Fitz <f18846188605@gmail.com>
2018-07-02 21:26:25 +0800

    修改了项目的文件结构

--- EasyTranslate/main.js
+++ EasyTranslate/main.js
@@ -1,11 +1,11 @@
-function onClickHandler(info, tab) {
-    var text = info.selectionText;
+function onClickHandler(clickedElement) {
+    var text = clickedElement.selectionText;
 
     var request = new XMLHttpRequest();
     request.open("GET", "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=zh&dt=t&dt=at&q=" + text, true);
     request.send();
-    request.onreadystatechange = function() {
-        if (request.readyState == 4){
+    request.onreadystatechange = function () {
+        if (request.readyState == 4) {
             var response = JSON.parse(request.response);
             var meanings = response[5][0][2];
             var meaning = "";
@@ -15,11 +15,11 @@ function onClickHandler(info, tab) {
             alert(meaning);
         }
     }
-  };
-  
-  chrome.contextMenus.onClicked.addListener(onClickHandler);
-  
-  // Set up context menu tree at install time.
-  chrome.runtime.onInstalled.addListener(function() {
-      chrome.contextMenus.create({"title": "Easy Translate", "contexts":["selection"], "id": "context_selection"});
-  });
+};
+
+chrome.contextMenus.onClicked.addListener(onClickHandler);
+
+// Set up context menu tree at install time.
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.contextMenus.create({ "title": "Easy Translate", "contexts": ["selection"], "id": "context_selection" });
+});
