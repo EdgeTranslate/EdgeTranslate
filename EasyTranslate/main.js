ea7878129c7f3ff26fb94f55e53dabe0a5129d48
Fitz <f18846188605@gmail.com>
2018-07-02 22:05:13 +0800

    修改了翻译api接口

--- EasyTranslate/main.js
+++ EasyTranslate/main.js
@@ -2,20 +2,26 @@ function onClickHandler(clickedElement) {
     var text = clickedElement.selectionText;
 
     var request = new XMLHttpRequest();
-    request.open("GET", "https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=zh&dt=t&dt=at&q=" + text, true);
+    request.open("GET", "https://translate.google.cn/translate_a/single?client=gtx&sl=en&tl=zh&dt=t&dt=at&q=" + text, true);
     request.send();
     request.onreadystatechange = function () {
-        if (request.readyState == 4) {
+        if (request.readyState == 4 && request.status == 200) {
+            console.log(response);
             var response = JSON.parse(request.response);
             var meanings = response[5][0][2];
             var meaning = "";
             for (var i = 0; i < meanings.length; i++) {
                 meaning += meanings[i][0] + ", ";
             }
+            showTranslate(clickedElement, meaning);
             alert(meaning);
         }
     }
 };
+
+var showTranslate = function (element, content) {
+    console.log(element);
+}
 
 chrome.contextMenus.onClicked.addListener(onClickHandler);
 
