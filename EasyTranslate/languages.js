4aa3c4f88d98de9dd7964e801468c1944c5e312d
nickyc975 <chenjinlong2016@gmail.com>
2018-07-03 13:27:21 +0800

    尝试实现保存语言选项的功能，目前无法读取存储内容。

--- EasyTranslate/languages.js
+++ EasyTranslate/languages.js
@@ -1,5 +1,6 @@
 var languages = [
-    {"name": "中文", "value": "zh"},
-    {"name": "英文", "value": "en"}
+    {"name": "中文（简体）", "value": "zh"},
+    {"name": "中文（繁体）", "value": "zh"},
+    {"name": "English", "value": "en"}
 ]
     
--- EasyTranslate/options.js
+++ EasyTranslate/options.js
@@ -1,3 +1,49 @@
-document.onload(function() {
+window.onload = function () {
+    var default_sl;
+    chrome.storage.local.get("sl", function (result) {
+        default_sl = result.key;
+        console.log(default_sl);
+    });
+    var default_tl;
+    chrome.storage.local.get("tl", function (result) {
+        default_tl = result.key;
+        console.log(default_tl);
+    });
     
-});
+    var source_language = document.getElementById("sl");
+    var target_language = document.getElementById("tl");
+
+    languages.forEach(element => {
+        if (element.value == default_sl) {
+            source_language.options.add(new Option(element.name, element.value, true, true));
+        } else {
+            source_language.options.add(new Option(element.name, element.value));
+        }
+
+        if (element.value == default_tl) {
+            target_language.options.add(new Option(element.name, element.value, true, true));
+        } else {
+            target_language.options.add(new Option(element.name, element.value));
+        }
+    });
+
+    source_language.onchange = function () {
+        saveOption("sl", source_language.options[source_language.selectedIndex].value);
+        console.log("sl = " + chrome.storage.local.get("sl", function (items) {
+            console.log("sl not found!");
+        }));
+    };
+
+    target_language.onchange = function () {
+        saveOption("tl", target_language.options[target_language.selectedIndex].value);
+        console.log("tl = " + chrome.storage.local.get("tl", function (items) {
+            console.log("tl not found!");
+        }));
+    };
+};
+
+function saveOption (key, value) {
+    chrome.storage.local.set({key: value}, function () {
+        console.log(key + ": " + value);
+    });
+};
