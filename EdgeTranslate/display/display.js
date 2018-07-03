ee617a7bca4a40bdffa421faaa021728f4b93909
Fitz <f18846188605@gmail.com>
2018-07-03 21:15:41 +0800

    在侧边栏的右上角添加了关闭按钮

--- EdgeTranslate/display/display.js
+++ EdgeTranslate/display/display.js
@@ -9,13 +9,20 @@ var display = function (content) {
 var createBlock = function (content) {
     frame = document.createElement('DIV');
     frame.id = 'translate_frame';
-    var p = document.createElement('p');
-    p.innerText = content;
-    frame.appendChild(p);
-    document.documentElement.appendChild(frame);
     document.body.style.transition = 'width 500ms';
     document.body.style.width = '85%';
     document.documentElement.addEventListener('click', clickListener)
+
+    var p = document.createElement('p');
+    p.innerText = content;
+
+    let closeIcon = document.createElement('i');
+    closeIcon.className = 'translate-icon-close';
+    closeIcon.onclick = removeSlider;
+
+    document.documentElement.appendChild(frame);
+    frame.appendChild(closeIcon);
+    frame.appendChild(p);
 }
 
 var isParentNode = function (node1, node2) {
@@ -31,9 +38,13 @@ var isParentNode = function (node1, node2) {
 var clickListener = function (event) {
     let node = event.target;
     if (!isParentNode(node, frame)) {
-        document.documentElement.removeChild(frame);
-        document.body.style.width = '100%';
-        document.documentElement.removeEventListener('click', clickListener);
+        removeSlider();
     }
+}
+
+var removeSlider = function () {
+    document.documentElement.removeChild(frame);
+    document.body.style.width = '100%';
+    document.documentElement.removeEventListener('click', clickListener);
 }
 chrome.runtime.onMessage.addListener(display);
