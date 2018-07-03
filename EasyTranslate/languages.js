b3ab9e0554f0a66ca5394ca7df8d098db65b56d3
nickyc975 <chenjinlong2016@gmail.com>
2018-07-03 18:11:07 +0800

    实现设置翻译语言的功能。

--- EasyTranslate/languages.js
+++ EasyTranslate/languages.js
@@ -1,6 +1,6 @@
 var languages = [
-    {"name": "中文（简体）", "value": "zh"},
-    {"name": "中文（繁体）", "value": "zh"},
+    {"name": "中文（简体）", "value": "zh-CN"},
+    {"name": "中文（繁体）", "value": "zh-TW"},
     {"name": "English", "value": "en"}
 ]
     
--- EasyTranslate/options.js
+++ EasyTranslate/options.js
@@ -1,49 +1,50 @@
 window.onload = function () {
-    var default_sl;
-    chrome.storage.local.get("sl", function (result) {
-        default_sl = result.key;
-        console.log(default_sl);
+    const language_setting = "language_setting";
+
+    // 获得用户之前选择的语言翻译选项。
+    chrome.storage.sync.get(language_setting, function (result) {
+        var source_language = document.getElementById("sl");
+        var target_language = document.getElementById("tl");
+
+        var default_language_setting = result[language_setting];
+
+        console.log("default_language_setting: " + default_language_setting);
+
+        languages.forEach(element => {
+            console.log("ele_value: " + element.value);
+            if (default_language_setting && element.value == default_language_setting.sl) {
+                source_language.options.add(new Option(element.name, element.value, true, true));
+            } else {
+                source_language.options.add(new Option(element.name, element.value));
+            }
+
+            if (default_language_setting && element.value == default_language_setting.tl) {
+                target_language.options.add(new Option(element.name, element.value, true, true));
+            } else {
+                target_language.options.add(new Option(element.name, element.value));
+            }
+        });
+
+        source_language.onchange = function () {
+            save_option(language_setting, {
+                "sl": source_language.options[source_language.selectedIndex].value,
+                "tl": target_language.options[target_language.selectedIndex].value
+            });
+        };
+
+        target_language.onchange = function () {
+            save_option(language_setting, {
+                "sl": source_language.options[source_language.selectedIndex].value,
+                "tl": target_language.options[target_language.selectedIndex].value
+            });
+        };
     });
-    var default_tl;
-    chrome.storage.local.get("tl", function (result) {
-        default_tl = result.key;
-        console.log(default_tl);
-    });
-    
-    var source_language = document.getElementById("sl");
-    var target_language = document.getElementById("tl");
 
-    languages.forEach(element => {
-        if (element.value == default_sl) {
-            source_language.options.add(new Option(element.name, element.value, true, true));
-        } else {
-            source_language.options.add(new Option(element.name, element.value));
-        }
 
-        if (element.value == default_tl) {
-            target_language.options.add(new Option(element.name, element.value, true, true));
-        } else {
-            target_language.options.add(new Option(element.name, element.value));
-        }
-    });
-
-    source_language.onchange = function () {
-        saveOption("sl", source_language.options[source_language.selectedIndex].value);
-        console.log("sl = " + chrome.storage.local.get("sl", function (items) {
-            console.log("sl not found!");
-        }));
-    };
-
-    target_language.onchange = function () {
-        saveOption("tl", target_language.options[target_language.selectedIndex].value);
-        console.log("tl = " + chrome.storage.local.get("tl", function (items) {
-            console.log("tl not found!");
-        }));
-    };
 };
 
-function saveOption (key, value) {
-    chrome.storage.local.set({key: value}, function () {
-        console.log(key + ": " + value);
-    });
+function save_option(key, value) {
+    var item = {};
+    item[key] = value;
+    chrome.storage.sync.set(item);
 };
