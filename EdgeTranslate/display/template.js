c6e30d207b2012de7587d9058ec920b393a676b0
nickyc975 <chenjinlong2016@gmail.com>
2018-07-08 17:44:59 +0800

    向模板中添加更多内容。

--- EdgeTranslate/display/template.js
+++ EdgeTranslate/display/template.js
@@ -1,3 +1,6 @@
 var template = '<i class="translate-icon-close"></i>' +
-               '<div class="mainMeaning"><% this.mainMeaning %></div>' + 
-               '<div class="commonMeanings"><% this.commonMeanings %></div>';
+               '<p class="mainMeaning"><% this.mainMeaning %></p>' + 
+               '<p class="commonMeanings"><% this.commonMeanings %></p>' + 
+               '<% for (var i in this.detailedMeanings) { %>' + 
+                   '<p><% this.detailedMeanings[i].type + ": " + this.detailedMeanings[i].meaning %></p>' + 
+               '<% } %>';
