df6d4c564f5512fbda23c22c78ac55eb4441da88
nickyc975 <chenjinlong2016@gmail.com>
2018-07-08 18:53:34 +0800

    在模板中添加近同义词。

--- EdgeTranslate/display/template.js
+++ EdgeTranslate/display/template.js
@@ -1,18 +1,26 @@
 var template = '<i class="translate-icon-close"></i>' + 
-               '<div class="main-meaning">' + 
-                   '<p class="main-meaning-title">主要意思：</p>' + 
-                   '<p class="main-meaning-content"><% this.mainMeaning %></p>' + 
+               '<div class="translate-main-meaning">' + 
+                   '<p class="translate-main-meaning-content"><% this.mainMeaning %></p>' + 
                '</div>' + 
-               '<div class="common-meanings">' + 
-                   '<p class="common-meanings-title">常见意思：</p>' + 
-                   '<p class="common-meanings-content"><% this.commonMeanings %></p>' + 
+               '<div class="translate-common-meanings">' + 
+                   '<p class="translate-common-meanings-title">常见意思：</p>' + 
+                   '<p class="translate-common-meanings-content"><% this.commonMeanings %></p>' + 
                '</div>' + 
-               '<div class="detailed-meanings">' + 
-                   '<% for (var i in this.detailedMeanings) { %>' + 
-                       '<p class="detailed-meanings-title">单词详解：</p>' + 
-                       '<p class="detailed-meanings-content">' + 
-                           '<span class="word-type"><% this.detailedMeanings[i].type %></span>' + 
-                           '<span class="word-meaning"><% ": " + this.detailedMeanings[i].meaning %></span>' + 
+               '<div class="translate-detailed-meanings">' + 
+                   '<p class="translate-detailed-meanings-title">单词详解：</p>' + 
+                   '<% for (let i in this.detailedMeanings) { %>' + 
+                       '<p class="translate-detailed-meanings-content">' + 
+                           '<span class="translate-detailed-meanings-word-type"><% this.detailedMeanings[i].type + ": " %></span>' + 
+                           '<span class="translate-detailed-meanings-word-meaning"><% this.detailedMeanings[i].meaning %></span>' + 
+                       '</p>' + 
+                   '<% } %>' + 
+               '</div>' + 
+               '<div class="translate-synonyms">' + 
+                   '<p class="translate-synonyms-title">近同义词：</p>' + 
+                   '<% for (let i in this.synonyms) { %>' + 
+                       '<p class="translate-synonyms-content">' + 
+                           '<span class="translate-synonyms-word-type"><% this.synonyms[i].type + ": " %></span>' + 
+                           '<span class="translate-synonyms-words"><% this.synonyms[i].words[0] %></span>' + 
                        '</p>' + 
                    '<% } %>' + 
                '</div>';
