9d408cd00676d15d2b47a23139fa752ada29245b
nickyc975 <chenjinlong2016@gmail.com>
2018-07-08 18:21:48 +0800

    为模板添加更多细节。

--- EdgeTranslate/display/template.js
+++ EdgeTranslate/display/template.js
@@ -1,6 +1,18 @@
-var template = '<i class="translate-icon-close"></i>' +
-               '<p class="mainMeaning"><% this.mainMeaning %></p>' + 
-               '<p class="commonMeanings"><% this.commonMeanings %></p>' + 
-               '<% for (var i in this.detailedMeanings) { %>' + 
-                   '<p><% this.detailedMeanings[i].type + ": " + this.detailedMeanings[i].meaning %></p>' + 
-               '<% } %>';
+var template = '<i class="translate-icon-close"></i>' + 
+               '<div class="main-meaning">' + 
+                   '<p class="main-meaning-title">主要意思：</p>' + 
+                   '<p class="main-meaning-content"><% this.mainMeaning %></p>' + 
+               '</div>' + 
+               '<div class="common-meanings">' + 
+                   '<p class="common-meanings-title">常见意思：</p>' + 
+                   '<p class="common-meanings-content"><% this.commonMeanings %></p>' + 
+               '</div>' + 
+               '<div class="detailed-meanings">' + 
+                   '<% for (var i in this.detailedMeanings) { %>' + 
+                       '<p class="detailed-meanings-title">单词详解：</p>' + 
+                       '<p class="detailed-meanings-content">' + 
+                           '<span class="word-type"><% this.detailedMeanings[i].type %></span>' + 
+                           '<span class="word-meaning"><% ": " + this.detailedMeanings[i].meaning %></span>' + 
+                       '</p>' + 
+                   '<% } %>' + 
+               '</div>';
