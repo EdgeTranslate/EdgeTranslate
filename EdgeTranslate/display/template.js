bab82de4aa4a99f54d6f0348756edff95c143e7d
nickyc975 <chenjinlong2016@gmail.com>
2018-07-16 16:27:17 +0800

    国际化应用，增加简中，繁中和英语翻译。

--- EdgeTranslate/display/template.js
+++ EdgeTranslate/display/template.js
@@ -17,7 +17,7 @@ var template =  '<i class="translate-icon translate-icon-tuding-fix translate-fi
                '</div>' + 
                '<div class="translate-common-meanings">' + 
                    '<% if (this.commonMeanings && this.commonMeanings.length > 0) { %>' + 
-                        '<p class="translate-content-title">常见意思:</p>' + 
+                        '<p class="translate-content-title"><% chrome.i18n.getMessage("CommonMeanings") %>: </p>' + 
                         '<ul class="translate-list">'+
                             '<li class="translate-common-meanings-content"><% this.commonMeanings %></li>' + 
                         '</ul>'+
@@ -25,7 +25,7 @@ var template =  '<i class="translate-icon translate-icon-tuding-fix translate-fi
                '</div>' + 
                '<div class="translate-detailed-meanings">' + 
                    '<% if (this.detailedMeanings && this.detailedMeanings.length > 0) { %>' + 
-                       '<p class="translate-content-title">单词详解：</p>' + 
+                       '<p class="translate-content-title"><% chrome.i18n.getMessage("DetailedMeanings") %>: </p>' + 
                        '<% for (let i in this.detailedMeanings) { %>' + 
                            '<p class="translate-detailed-meanings-content">' + 
                                '<span class="translate-word-type"><% this.detailedMeanings[i].type + ": " %></span>' + 
@@ -36,16 +36,16 @@ var template =  '<i class="translate-icon translate-icon-tuding-fix translate-fi
                '</div>' + 
                '<div class="translate-definitions">' + 
                    '<% if (this.definitions && this.definitions.length > 0) { %>' + 
-                       '<p class="translate-content-title">单词定义：</p>' + 
+                       '<p class="translate-content-title"><% chrome.i18n.getMessage("Definitions") %>: </p>' + 
                        '<% for (let i in this.definitions) { %>' + 
                            '<div class="translate-definitions-content">' + 
                                '<p class="translate-word-type"><% this.definitions[i].type + ": " %></p>' + 
                                '<ul class="translate-list">' + 
                                    '<% for (let j in this.definitions[i].meanings) { %>' + 
                                        '<li>' + 
-                                           '<span class="translate-word-type" >释义：</span>'+
+                                           '<span class="translate-word-type"><% chrome.i18n.getMessage("DefinitionsMeaning") %>: </span>'+
                                            '<span class="translate-content"><% this.definitions[i].meanings[j].meaning %></span>' + 
-                                           '<span class="translate-word-type">示例：</span>'+
+                                           '<span class="translate-word-type"><% chrome.i18n.getMessage("DefinitionsExample") %>: </span>'+
                                            '<span class="translate-content"><% this.definitions[i].meanings[j].example %></span>' + 
                                        '</li>' + 
                                    '<% } %>' + 
@@ -56,7 +56,7 @@ var template =  '<i class="translate-icon translate-icon-tuding-fix translate-fi
                '</div>' + 
                '<div class="translate-synonyms">' + 
                    '<% if (this.synonyms && this.synonyms.length > 0) { %>' + 
-                       '<p class="translate-content-title">近同义词：</p>' + 
+                       '<p class="translate-content-title"><% chrome.i18n.getMessage("Synonyms") %>: </p>' + 
                        '<% for (let i in this.synonyms) { %>' + 
                            '<p class="translate-synonyms-content">' + 
                                '<span class="translate-word-type"><% this.synonyms[i].type + ": " %></span>' + 
@@ -67,7 +67,7 @@ var template =  '<i class="translate-icon translate-icon-tuding-fix translate-fi
                '</div>' + 
                '<div class="translate-phrases">' + 
                    '<% if (this.phrases && this.phrases.length > 0) { %>' + 
-                       '<p class="translate-content-title">常用短语：</p>' + 
+                       '<p class="translate-content-title"><% chrome.i18n.getMessage("Phrases") %>: </p>' + 
                        '<ul class="translate-list">' + 
                            '<% for (let i in this.phrases) { %>' + 
                                '<li class="translate-content"><% this.phrases[i] %></li>' + 
@@ -77,7 +77,7 @@ var template =  '<i class="translate-icon translate-icon-tuding-fix translate-fi
                '</div>' + 
                '<div class="translate-examples">' + 
                    '<% if (this.examples && this.examples.length > 0) { %>' + 
-                       '<p class="translate-content-title">典型例句：</p>' + 
+                       '<p class="translate-content-title"><% chrome.i18n.getMessage("Examples") %>: </p>' + 
                        '<p>' + 
                            '<% for (let i in this.examples) { %>' + 
                                '<p class="translate-sentences"><% this.examples[i] %></p>' + 
