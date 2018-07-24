da132d00b9befec3f65bdea22cb5de19dfe174f0
Fitz <f18846188605@gmail.com>
2018-07-24 18:27:53 +0800

    调整了样式 修复在英语提示下的显示问题

--- EdgeTranslate/options/options.js
+++ EdgeTranslate/options/options.js
@@ -1,5 +1,5 @@
 /**
- * 初始化设置列表。
+ * 初始化设置列表�?
  */
 window.onload = function () {
     var i18nElemwnts = document.getElementsByClassName("i18n");
@@ -8,11 +8,11 @@ window.onload = function () {
         i18nElemwnts[i].insertAdjacentText("beforeEnd", chrome.i18n.getMessage(i18nElemwnts[i].getAttribute("data-i18n-name")));
     }
 
-    // 获得用户之前选择的语言翻译选项。
+    // 获得用户之前选择的语言翻译选项�?
     chrome.storage.sync.get("languageSetting", function (result) {
         var languageSetting = result.languageSetting;
 
-        // 获取下拉列表元素。
+        // 获取下拉列表元素�?
         var sourceLanguage = document.getElementById("sl");
         var targetLanguage = document.getElementById("tl");
         // 获取交换按钮
@@ -28,7 +28,7 @@ window.onload = function () {
             }
         }
 
-        // languages是可选的源语言和目标语言的列表。
+        // languages是可选的源语言和目标语言的列表�?
         LANGUAGES.forEach(element => {
             if (languageSetting && element.value == languageSetting.sl) {
                 sourceLanguage.options.add(new Option(element.name, element.value, true, true));
@@ -47,7 +47,7 @@ window.onload = function () {
         judgeValue(exchangeButton, sourceLanguage);
 
         sourceLanguage.onchange = function () {
-            // 如何源语言是自动判断语言类型(值是auto),则按钮显示灰色，避免用户点击,如果不是，则显示蓝色，可以点击
+            // 如何源语言是自动判断语言类型(值是auto),则按钮显示灰色，避免用户点击,如果不是，则显示蓝色，可以点�?
             judgeValue(exchangeButton, sourceLanguage);
             updateLanguageSetting(
                 sourceLanguage.options[sourceLanguage.selectedIndex].value,
@@ -70,14 +70,14 @@ window.onload = function () {
         var configCheckbox = [];
 
         //添加翻译选项的选择元素
-        configCheckbox.push(document.getElementById('ex'));  // 显示例句选项框
-        configCheckbox.push(document.getElementById('ss'));  // 显示相关词选项框
-        configCheckbox.push(document.getElementById('md'));  // 显示定义选项框
-        configCheckbox.push(document.getElementById('rw'));  // 显示词组选项框
-        configCheckbox.push(document.getElementById('bd'));  // 显示所有含义选项框
-        configCheckbox.push(document.getElementById('at'));  // 显示常用意思选项框
+        configCheckbox.push(document.getElementById('ex'));  // 显示例句选项�?
+        configCheckbox.push(document.getElementById('ss'));  // 显示相关词选项�?
+        configCheckbox.push(document.getElementById('md'));  // 显示定义选项�?
+        configCheckbox.push(document.getElementById('rw'));  // 显示词组选项�?
+        configCheckbox.push(document.getElementById('bd'));  // 显示所有含义选项�?
+        configCheckbox.push(document.getElementById('at'));  // 显示常用意思选项�?
 
-        // 首先将初始化的设置同步到页面中
+        // 首先将初始化的设置同步到页面�?
         for (let i = 0; i < configCheckbox.length; i++)
             configCheckbox[i].checked = DTSetting.indexOf(configCheckbox[i].value) !== -1;
 
@@ -85,9 +85,9 @@ window.onload = function () {
         for (let i = 0; i < configCheckbox.length; i++)
             configCheckbox[i].onchange = function () {
                 // this 表示的当前的筛选框元素
-                if (this.checked) // 用户勾选了这一项
+                if (this.checked) // 用户勾选了这一�?
                     DTSetting.push(this.value);
-                else // 用户删除了这一项
+                else // 用户删除了这一�?
                     DTSetting.splice(DTSetting.indexOf(this.value), 1);
                 // 同步修改后的设定
                 updateTranslateSetting(DTSetting);
@@ -98,8 +98,8 @@ window.onload = function () {
      * 
      * 如果源语言是自动判断语言类型(值是auto),则按钮显示灰色，避免用户点击
      * 
-     * @param {*HTMLElement} exchangeButton 特定的一个element,是一个交换按钮图标
-     * @param {*HTMLElement} sourceLanguage 特定的一个element,源语言的选项框
+     * @param {*HTMLElement} exchangeButton 特定的一个element,是一个交换按钮图�?
+     * @param {*HTMLElement} sourceLanguage 特定的一个element,源语言的选项�?
      */
     var judgeValue = function (exchangeButton, sourceLanguage) {
         if (sourceLanguage.value === 'auto')
@@ -113,7 +113,7 @@ window.onload = function () {
 };
 
 /**
- * 保存翻译语言设定。
+ * 保存翻译语言设定�?
  * 
  * @param {*} sourceLanguage 源语言
  * @param {*} targetLanguage 目标语言
@@ -123,7 +123,7 @@ function updateLanguageSetting(sourceLanguage, targetLanguage) {
 }
 
 /**
- * 保存翻译选项设置(DTSetting)。
+ * 保存翻译选项设置(DTSetting)�?
  * 
  * @param {*object} DTSetting 需要同步的翻译选项设定
  */
@@ -132,10 +132,10 @@ function updateTranslateSetting(DTSetting) {
 }
 
 /**
- * 保存一条设置项。
+ * 保存一条设置项�?
  * 
  * @param {*} key 设置项名
- * @param {*} value 设置项值
+ * @param {*} value 设置项�?
  */
 function saveOption(key, value) {
     var item = {};
@@ -153,7 +153,7 @@ function addEventListener() {
 
 /**
  * block start
- * 事件监听的回调函数定义请在此区域中进行
+ * 事件监听的回调函数定义请在此区域中进�?
  */
 
 /**
@@ -161,13 +161,13 @@ function addEventListener() {
  */
 function translateSubmit() {
     var content = document.getElementById('translate_input').value;
-    if (content.replace(/\s*/, '') !== '') { // 判断值是否为空
+    if (content.replace(/\s*/, '') !== '') { // 判断值是否为�?
         document.getElementById('hint_message').style.display = 'none';
         translate(content, function () {
             window.close(); // 展示结束后关闭option页面
         });
     }
-    else  // 提示输入的内容是空
+    else  // 提示输入的内容是�?
         document.getElementById('hint_message').style.display = 'inline';
 }
 
