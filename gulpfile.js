2005fc8cbfda57bce5d575b0cd58d5722d8d6b6f
Fitz <f18846188605@gmail.com>
2018-08-13 15:49:03 +0800

    添加npm scripts 添加了开发环境下的热更新安装包的命令，便于开发

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ const through = require("through2");
 const webpack = require("webpack");
 const webpack_stream = require("webpack-stream");
 const zip = require("gulp-zip");
+var cached = require("gulp-cached");
+var remember = require("gulp-remember");
 
 /**
  * 清除之前打包好的chrome的缓存
@@ -29,18 +31,48 @@ gulp.task("clean:firefox", function (callback) {
 });
 
 /**
- * build chrome版扩展
+ * 发开环境下热更新 chrome扩展的安装包
  */
-gulp.task("build:chrome", function (callback) {
-    build("chrome");
+gulp.task("watcher:chrome", function (callback) {
+    watcher("chrome");
     callback();
 });
 
 /**
- * build firefox版扩展
+ * 发开环境下热更新 firefox扩展的安装包
+ */
+gulp.task("watcher:firefox", function (callback) {
+    watcher("firefox");
+    callback();
+});
+
+/**
+ * 发开环境下build chrome扩展的安装包
+ */
+gulp.task("dev:chrome", function () {
+    build("chrome", "development");
+});
+
+/**
+ * 发开环境下build firefox扩展的安装包
+ */
+gulp.task("dev:firefox", function () {
+    build("firefox", "development");
+})
+
+/**
+ * 生产环境下build chrome版扩展
+ */
+gulp.task("build:chrome", function (callback) {
+    build("chrome", "production");
+    callback();
+});
+
+/**
+ * 生产环境下build firefox版扩展
  */
 gulp.task("build:firefox", function (callback) {
-    build("firefox");
+    build("firefox", "production");
     callback();
 });
 
@@ -65,27 +97,51 @@ gulp.task("pack:firefox", function (callback) {
 });
 
 /**
+ * 根据传入的参数执行不同的热更新场景
+ * 
+ * @param {String} browser 浏览器的名称
+ */
+function watcher(browser) {
+    var watcher = gulp.watch("./src/**/*", ["dev:" + browser]); // 监视src中所有文件
+    watcher.on("change", function (event) {
+        if (event.type === "deleted") {                   // 如果一个文件被删除了，则将其忘记
+            delete cached.caches["dev:" + browser][event.path];       // gulp-cached 的删除 api
+            remember.forget("dev:" + browser, event.path);         // gulp-remember 的删除 api
+        }
+    });
+}
+
+/**
  * 根据传入的参数build对应版本的扩展
  * 
- * @param {String} browser 
+ * @param {String} browser 浏览器的名称
+ * @param {String} env build的环境变量  (production/development)两种值
  */
-function build(browser) {
+function build(browser, env) {
     let output_dir = "./build/" + browser + "/";
     let manifest_patch = "./src/manifest_" + browser + ".json";
-    let webpack_path = "./config/webpack.dev.config.js"; // webpack 配置文件路径
+    let webpack_path = (env === 'production' ? "./config/webpack.prod.config.js" : "./config/webpack.dev.config.js"); // webpack 配置文件路径
 
     gulp.src("./src/**/*.js", { base: "src" })
+        .pipe(cached("build:" + browser))
         .pipe(webpack_stream(require(webpack_path), webpack))
+        .pipe(remember("build:" + browser))
         .pipe(gulp.dest(output_dir));
 
     gulp.src("./src/manifest.json", { base: "src" })
+        .pipe(cached("build:" + browser))
         .pipe(merge_json(manifest_patch))
+        .pipe(remember("build:" + browser))
         .pipe(gulp.dest(output_dir));
 
     gulp.src("./src/**/*.html", { base: "src" })
+        .pipe(cached("build:" + browser))
+        .pipe(remember("build:" + browser))
         .pipe(gulp.dest(output_dir));
 
     gulp.src("./static/**/*", { base: "static" })
+        .pipe(cached("build:" + browser))
+        .pipe(remember("build:" + browser))
         .pipe(gulp.dest(output_dir));
 }
 
@@ -112,3 +168,4 @@ function merge_json() {
 
     return stream;
 }
+
