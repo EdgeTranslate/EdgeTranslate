9d9b1a691414b969ba987a8c841ec8ed3e7b714d
nickyc975 <chenjinlong2016@gmail.com>
2019-02-28 12:20:04 +0800

    Format webpack config files

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -2,24 +2,24 @@
 const path = require("path");
 
 module.exports = {
-    entry: {
-        background: "./src/background.js",
-        "/contents/pdf": "./src/contents/pdf.js",
-        "/contents/select": "./src/contents/select.js",
-        "/popup/popup": "./src/popup/popup.js",
-        "/options/options": "./src/options/options.js",
-        "/display/display": "./src/display/display.js"
-    },
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "build")
-    },
-    module: {
-        rules: [
-            {
-                test: /\.html$/,
-                use: "raw-loader"
-            }
-        ]
-    }
+	entry: {
+		background: "./src/background.js",
+		"/contents/pdf": "./src/contents/pdf.js",
+		"/contents/select": "./src/contents/select.js",
+		"/popup/popup": "./src/popup/popup.js",
+		"/options/options": "./src/options/options.js",
+		"/display/display": "./src/display/display.js"
+	},
+	output: {
+		filename: "[name].js",
+		path: path.resolve(__dirname, "build")
+	},
+	module: {
+		rules: [
+			{
+				test: /\.html$/,
+				use: "raw-loader"
+			}
+		]
+	}
 };
--- config/webpack.dev.config.js
+++ config/webpack.dev.config.js
@@ -1,8 +1,8 @@
-'use strict'
-const merge = require('webpack-merge');
-const baseWebpackConfig = require('./webpack.base.config.js');
+"use strict";
+const merge = require("webpack-merge");
+const baseWebpackConfig = require("./webpack.base.config.js");
 
 module.exports = merge(baseWebpackConfig, {
-    mode: "development",
-    devtool: 'inline-source-map'
-})
+	mode: "development",
+	devtool: "inline-source-map"
+});
