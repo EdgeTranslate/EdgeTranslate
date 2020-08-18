c22a4770e21a02f1201203bd45fd57079e5fe2b2
Mark-fenng <f18846188605@gmail.com>
2020-08-18 16:53:56 +0800

    Added axios mock adapter module mock axios requests to avoid crossing origin and errors caused by axios' http adapter

--- test/translators/baidu.test.js
+++ test/translators/baidu.test.js
@@ -1,13 +1,14 @@
-// import axios from "axios";
+import axios from "axios";
 import fs from "fs";
+import mockAdapter from "axios-mock-adapter";
 import TRANSLATOR from "../../src/translators/baidu.js";
 
 describe("baidu translator api", () => {
     beforeEach(() => {
         // set http module of nodejs as axios' request method
-        // let path = require("path");
-        // let lib = path.join(path.dirname(require.resolve("axios")), "lib/adapters/http");
-        // axios.defaults.adapter = require(lib);
+        let path = require("path");
+        let lib = path.join(path.dirname(require.resolve("axios")), "lib/adapters/http");
+        axios.defaults.adapter = require(lib);
     });
 
     it("to detect language type", done => {
@@ -38,7 +39,31 @@ describe("baidu translator api", () => {
     });
 
     it("to translate a word", done => {
-        TRANSLATOR.translate("hello", "en", "zh")
+        TRANSLATOR.getTokenGtk()
+            .then(() => {
+                // translation request using axios' http adapter always return errors
+                // I have to mock requests of axios to avoid crossing origin and the errors
+                let mock = new mockAdapter(axios);
+                let query = "hello",
+                    from = "en",
+                    to = "zh",
+                    url = "/v2transapi?" + "from=" + from + "&to=" + to;
+                mock.onPost(url).reply(config => {
+                    // to check post data
+                    let data = new URLSearchParams(config.data);
+                    expect(data.get("from")).toEqual(from);
+                    expect(data.get("to")).toEqual(to);
+                    expect(data.get("query")).toEqual(query);
+                    expect(data.get("sign")).not.toEqual("");
+                    expect(data.get("token")).not.toEqual("");
+
+                    // to return translation result stored at $localResultPath
+                    let localResultPath = "test/translators/baiduTransResult.json";
+                    let result = fs.readFileSync(localResultPath, "utf-8").toString();
+                    return [200, result];
+                });
+                return TRANSLATOR.translate(query, from, to);
+            })
             .then(result => {
                 let resultObject = result.data;
                 expect(resultObject.trans_result).toBeDefined();
