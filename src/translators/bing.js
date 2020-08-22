00b6fce18f8e9a9f756dda67cc0d02f541216ac8
Nicky Chen <chenjinlong2016@outlook.com>
2020-08-22 22:46:10 +0800

    Implemented Bing translator TTS interface.

--- src/translators/bing.js
+++ src/translators/bing.js
@@ -85,6 +85,84 @@ const LANGUAGES = [
 ];
 
 /**
+ * Text readers.
+ */
+const READERS = {
+    ar: ["ar-SA", "Male", "ar-SA-Naayf"],
+    bg: ["bg-BG", "Male", "bg-BG-Ivan"],
+    ca: ["ca-ES", "Female", "ca-ES-HerenaRUS"],
+    cs: ["cs-CZ", "Male", "cs-CZ-Jakub"],
+    da: ["da-DK", "Female", "da-DK-HelleRUS"],
+    de: ["de-DE", "Female", "de-DE-Hedda"],
+    el: ["el-GR", "Male", "el-GR-Stefanos"],
+    en: ["en-US", "Female", "en-US-JessaRUS"],
+    es: ["es-ES", "Female", "es-ES-Laura-Apollo"],
+    fi: ["fi-FI", "Female", "fi-FI-HeidiRUS"],
+    fr: ["fr-FR", "Female", "fr-FR-Julie-Apollo"],
+    he: ["he-IL", "Male", "he-IL-Asaf"],
+    hi: ["hi-IN", "Female", "hi-IN-Kalpana-Apollo"],
+    hr: ["hr-HR", "Male", "hr-HR-Matej"],
+    hu: ["hu-HU", "Male", "hu-HU-Szabolcs"],
+    id: ["id-ID", "Male", "id-ID-Andika"],
+    it: ["it-IT", "Male", "it-IT-Cosimo-Apollo"],
+    ja: ["ja-JP", "Female", "ja-JP-Ayumi-Apollo"],
+    ko: ["ko-KR", "Female", "ko-KR-HeamiRUS"],
+    ms: ["ms-MY", "Male", "ms-MY-Rizwan"],
+    nl: ["nl-NL", "Female", "nl-NL-HannaRUS"],
+    nb: ["nb-NO", "Female", "nb-NO-HuldaRUS"],
+    no: ["nb-NO", "Female", "nb-NO-HuldaRUS"],
+    pl: ["pl-PL", "Female", "pl-PL-PaulinaRUS"],
+    pt: ["pt-PT", "Female", "pt-PT-HeliaRUS"],
+    "pt-pt": ["pt-PT", "Female", "pt-PT-HeliaRUS"],
+    ro: ["ro-RO", "Male", "ro-RO-Andrei"],
+    ru: ["ru-RU", "Female", "ru-RU-Irina-Apollo"],
+    sk: ["sk-SK", "Male", "sk-SK-Filip"],
+    sl: ["sl-SL", "Male", "sl-SI-Lado"],
+    sv: ["sv-SE", "Female", "sv-SE-HedvigRUS"],
+    ta: ["ta-IN", "Female", "ta-IN-Valluvar"],
+    te: ["te-IN", "Male", "te-IN-Chitra"],
+    th: ["th-TH", "Male", "th-TH-Pattara"],
+    tr: ["tr-TR", "Female", "tr-TR-SedaRUS"],
+    vi: ["vi-VN", "Male", "vi-VN-An"],
+    "zh-Hans": ["zh-CN", "Female", "zh-CN-HuihuiRUS"],
+    "zh-Hant": ["zh-CN", "Female", "zh-CN-HuihuiRUS"],
+    yue: ["zh-HK", "Female", "zh-HK-TracyRUS"]
+};
+
+/**
+ * TTS language code.
+ */
+const TTS_LAN_CODE = {
+    ar: "ar-EG",
+    ca: "ca-ES",
+    da: "da-DK",
+    de: "de-DE",
+    en: "en-US",
+    es: "es-ES",
+    fi: "fi-FI",
+    fr: "fr-FR",
+    hi: "hi-IN",
+    it: "it-IT",
+    ja: "ja-JP",
+    ko: "ko-KR",
+    nb: "nb-NO",
+    nl: "nl-NL",
+    pl: "pl-PL",
+    pt: "pt-PT",
+    ru: "ru-RU",
+    sv: "sv-SE",
+    th: "th-TH",
+    "zh-Hans": "zh-CN",
+    "zh-Hant": "zh-HK",
+    yue: "zh-HK",
+    gu: "gu-IN",
+    mr: "mr-IN",
+    ta: "ta-IN",
+    te: "te-IN",
+    tr: "tr-TR"
+};
+
+/**
  * Bing translator interface.
  */
 class BingTranslator {
@@ -128,6 +206,11 @@ class BingTranslator {
          * Translator language code to language.
          */
         this.CODE_TO_LAN = new Map(LANGUAGES.map(([lan, code]) => [code, lan]));
+
+        /**
+         * Audio instance.
+         */
+        this.AUDIO = new Audio();
     }
 
     /**
@@ -211,30 +294,63 @@ class BingTranslator {
     }
 
     /**
+     * Get TTS auth token.
+     *
+     * @returns {Promise<Object>} Promise of auth data
+     */
+    getTTSAuth() {
+        return this.request({
+            method: "POST",
+            baseURL: this.HOST,
+            url:
+                "tfetspktok?isVertical=1&&IG=" +
+                this.IG +
+                "&IID=" +
+                this.IID +
+                "." +
+                this.count.toString(),
+            headers: this.HEADERS,
+            data: ""
+        });
+    }
+
+    /**
+     * Transform binary data into Base64 encoding.
+     *
+     * @param {ArrayBuffer} buffer array buffer with audio data
+     *
+     * @returns {String} Base64 form of binary data in buffer
+     */
+    arrayBufferToBase64(buffer) {
+        let str = "",
+            array = new Uint8Array(buffer);
+
+        for (let i = 0; i < array.byteLength; i++) {
+            str += String.fromCharCode(array[i]);
+        }
+
+        return btoa(str);
+    }
+
+    /**
      * Request APIs.
      *
-     * @param {String} method method to use
-     * @param {String} url url to request
-     * @param {String} data data to send
+     * This is a wrapper of axios with retrying and error handling supported.
+     *
+     * @param {Object} params request parameters
      * @param {Boolean} retry whether retry is needed
      *
      * @returns {Promise<Object>} Promise of response data
      */
-    async request(method, url, data, retry = true) {
+    async request(params, retry = true) {
         let retryCount = 0;
         let requestOnce = () => {
             return new Promise((resolve, reject) => {
                 this.count++;
-                axios({
-                    url: url,
-                    method: method,
-                    baseURL: this.HOST,
-                    headers: this.HEADERS,
-                    data: data
-                })
+                axios(params)
                     .then(response => {
                         // response.data.statusCode will indicate the info of error when error encountered
-                        if (response.data.statusCode) {
+                        if (response.data.statusCode && response.data.statusCode > 299) {
                             // Retry after failure
                             if (retry && retryCount < this.MAX_RETRY) {
                                 retryCount++;
@@ -286,7 +402,13 @@ class BingTranslator {
             data = "&fromLang=auto-detect&to=zh-Hans&text=" + encodeURIComponent(text);
 
         return (
-            this.request("POST", url, data)
+            this.request({
+                method: "POST",
+                baseURL: this.HOST,
+                url: url,
+                headers: this.HEADERS,
+                data: data
+            })
                 .then(response => {
                     let result = response[0].detectedLanguage.language;
                     return this.CODE_TO_LAN.get(result);
@@ -330,7 +452,13 @@ class BingTranslator {
 
         return (
             // Request the translate API firstly.
-            this.request("POST", translateURL, translateData)
+            this.request({
+                method: "POST",
+                baseURL: this.HOST,
+                url: translateURL,
+                headers: this.HEADERS,
+                data: translateData
+            })
                 .then(async transResponse => {
                     let lookupURL =
                             "tlookupv3?isVertical=1&IG=" +
@@ -355,9 +483,13 @@ class BingTranslator {
                     try {
                         // Attempt to request the lookup API for detailed translation.
                         const lookupResponse = await this.request(
-                            "POST",
-                            lookupURL,
-                            lookupData,
+                            {
+                                method: "POST",
+                                baseURL: this.HOST,
+                                url: lookupURL,
+                                headers: this.HEADERS,
+                                data: lookupData
+                            },
                             false
                         );
                         return this.parseLookupResult(lookupResponse, transResult);
@@ -368,6 +500,86 @@ class BingTranslator {
                 // eslint-disable-next-line no-console
                 .catch(error => console.log(error))
         );
+    }
+
+    /**
+     * Pronounce given text.
+     *
+     * @param {String} text text to pronounce
+     * @param {String} language language of text
+     * @param {String} speed "fast" or "slow"
+     *
+     * @returns {Promise} pronounce finished
+     */
+    pronounce(text, language, speed) {
+        // Pause audio in case that it's playing.
+        this.stopPronounce();
+
+        return (
+            this.getTTSAuth()
+                .then(authResponse => {
+                    let url =
+                        "https://" +
+                        authResponse.region +
+                        ".tts.speech.microsoft.com/cognitiveservices/v1?";
+
+                    let lanCode = this.LAN_TO_CODE.get(language);
+                    let reader = READERS[lanCode];
+                    let ttsLanCode = TTS_LAN_CODE[lanCode];
+                    let speedValue = speed === "fast" ? "-10.00%" : "-30.00%";
+                    let data =
+                        "<speak version='1.0' xml:lang='" +
+                        ttsLanCode +
+                        "'><voice xml:lang='" +
+                        ttsLanCode +
+                        "' xml:gender='" +
+                        reader[1] +
+                        "' name='" +
+                        reader[2] +
+                        "'><prosody rate='" +
+                        speedValue +
+                        "'>" +
+                        text +
+                        "</prosody></voice></speak>";
+
+                    let headers = {
+                        "Content-Type": "application/ssml+xml",
+                        Authorization: "Bearer " + authResponse.token,
+                        "X-MICROSOFT-OutputFormat": "audio-16khz-32kbitrate-mono-mp3",
+                        "cache-control": "no-cache"
+                    };
+
+                    return (
+                        this.request({
+                            method: "POST",
+                            baseURL: this.HOST,
+                            url: url,
+                            headers: headers,
+                            data: data,
+                            responseType: "arraybuffer"
+                        })
+                            .then(TTSResponse => {
+                                this.AUDIO.src =
+                                    "data:audio/mp3;base64," +
+                                    this.arrayBufferToBase64(TTSResponse);
+                                this.AUDIO.play();
+                            })
+                            // eslint-disable-next-line no-console
+                            .catch(error => console.log(error))
+                    );
+                })
+                // eslint-disable-next-line no-console
+                .catch(error => console.log(error))
+        );
+    }
+
+    /**
+     * Pause pronounce.
+     */
+    stopPronounce() {
+        if (!this.AUDIO.paused) {
+            this.AUDIO.pause();
+        }
     }
 }
 
