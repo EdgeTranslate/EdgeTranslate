86043fb6e19f2edc807c5b21d03f43e9b9fb1ccb
Nicky Chen <chenjinlong2016@outlook.com>
2020-08-23 18:11:29 +0800

    Improved Bing translator implementation. TTS will be faster and request count will be reset after updating IG and IID.

--- test/translators/bing.test.js
+++ test/translators/bing.test.js
@@ -13,8 +13,8 @@ describe("bing translator api", () => {
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36";
     });
 
-    it("to get IG and IID", done => {
-        TRANSLATOR.getIGIID()
+    it("to update IG and IID", done => {
+        TRANSLATOR.updateIGIID()
             .then(() => {
                 expect(typeof TRANSLATOR.IG).toEqual("string");
                 expect(TRANSLATOR.IG.length).toBeGreaterThan(0);
