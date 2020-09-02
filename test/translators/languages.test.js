a24203a3c7f145628ac58ba6c5f62c58c183a3bc
Nicky Chen <chenjinlong2016@outlook.com>
2020-09-02 21:17:53 +0800

    Update tests.

--- test/translators/languages.test.js
+++ test/translators/languages.test.js
@@ -2,6 +2,7 @@ import allLanguages from "../../src/translators/languages.js";
 import googleTranslator from "../../src/translators/google.js";
 import bingTranslator from "../../src/translators/bing.js";
 import baiduTranslator from "../../src/translators/baidu.js";
+import tencentTranslator from "../../src/translators/tencent.js";
 
 describe("these tests are used to make sure all of the supported languages in translators/**.js can be found in languages.js", () => {
     it("to test supported languages in google.js", done => {
@@ -26,6 +27,16 @@ describe("these tests are used to make sure all of the supported languages in tr
 
     it("to test supported languages in baidu.js", done => {
         let languages = baiduTranslator.supportedLanguages();
+        for (let lan of languages) {
+            if (lan !== "auto") {
+                expect(allLanguages[lan]).toBeDefined();
+            }
+        }
+        done();
+    });
+
+    it("to test supported languages in tencent.js", done => {
+        let languages = tencentTranslator.supportedLanguages();
         for (let lan of languages) {
             if (lan !== "auto") {
                 expect(allLanguages[lan]).toBeDefined();
