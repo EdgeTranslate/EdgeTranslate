85356bfc8cf891831a14d769f6a0d7e1850f54a5
Nicky Chen <chenjinlong2016@outlook.com>
2020-09-13 14:29:22 +0800

    Fixed a problem when translating in pdf.js pdf viewer.

--- src/content/display/display.js
+++ src/content/display/display.js
@@ -479,9 +479,9 @@ function getScrollbarWidth() {
     var scrollDiv = document.createElement("div");
     scrollDiv.style.cssText =
         "width: 99px; height: 99px; overflow: scroll; position: absolute; top: -9999px;";
-    document.body.appendChild(scrollDiv);
+    document.documentElement.appendChild(scrollDiv);
     var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
-    document.body.removeChild(scrollDiv);
+    document.documentElement.removeChild(scrollDiv);
     return scrollbarWidth;
 }
 
