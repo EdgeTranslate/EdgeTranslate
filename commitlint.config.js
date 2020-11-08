07a750b3d0b3eb3ef61e3f8cfba6c66f409f3910
Nicky Chen <chenjinlong2016@outlook.com>
2020-11-08 22:13:45 +0800

    build(commitlint): add commit type "improve"

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,23 @@
 module.exports = {
-    extends: ["@commitlint/config-conventional"]
+    extends: ["@commitlint/config-conventional"],
+    rules: {
+        "type-enum": [
+            2,
+            "always",
+            [
+                "build",
+                "chore",
+                "ci",
+                "docs",
+                "feat",
+                "fix",
+                "improve",
+                "perf",
+                "refactor",
+                "revert",
+                "style",
+                "test"
+            ]
+        ]
+    }
 };
