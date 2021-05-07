EdgeTranslate/EdgeTranslate

commit 1211ae83cbcac031fc83be81c0b268b397068e1c

improve: set examples as the innerHTML in a safe way and format the source and target results

src/content/display/Panel.jsx
@@ -142,7 +142,6 @@ export default function ResultPanel() {
                 window.translateResult.originalText = detail.text;
                 setOpen(true);
                 setContentType("LOADING");
-                setContent(detail);
             }
         });
 

src/content/display/Result.jsx
@@ -4,6 +4,7 @@ import { useEffect, useRef, useReducer } from "preact/hooks";
 import styled from "styled-components";
 import Channel from "common/scripts/channel.js";
 import Notifier from "./library/notifier/notifier.js";
+import DOMPurify from "dompurify";
 import { CommonPrefix, checkTimestamp } from "./Panel.jsx";
 import EditIcon from "./icons/edit.svg";
 import EditDoneIcon from "./icons/edit-done.svg";
@@ -94,7 +95,7 @@ export default function Result(props) {
         <Fragment>
             <Source>
                 <TextLine>
-                    <div class={`${CommonPrefix}may-need-rtl`}>{props.originalText}</div>
+                    <div class={`${CommonPrefix}may-need-rtl`}>{format(props.originalText)}</div>
                     <StyledEditIcon />
                     <StyledEditDoneIcon />
                 </TextLine>
@@ -105,7 +106,7 @@ export default function Result(props) {
                         <StyledPronounceIcon onClick={() => setSourcePronounce(true)} />
                     )}
                     <PronounceText class={`${CommonPrefix}may-need-rtl`}>
-                        {props.sPronunciation}
+                        {format(props.sPronunciation)}
                     </PronounceText>
                 </PronounceLine>
             </Source>
@@ -117,7 +118,7 @@ export default function Result(props) {
                         onBlur={() => setCopyResult({ copy: false })}
                         ref={translateResultElRef}
                     >
-                        {props.mainMeaning}
+                        {format(props.mainMeaning)}
                     </div>
                     <StyledCopyIcon
                         onClick={() =>
@@ -135,7 +136,7 @@ export default function Result(props) {
                         <StyledPronounceIcon onClick={() => setTargetPronounce(true)} />
                     )}
                     <PronounceText class={`${CommonPrefix}may-need-rtl`}>
-                        {props.tPronunciation}
+                        {format(props.tPronunciation)}
                     </PronounceText>
                 </PronounceLine>
             </Target>
@@ -208,9 +209,24 @@ export default function Result(props) {
                             {props.examples.map((example) => (
                                 <ExampleItem>
                                     {example.source && (
-                                        <ExampleSource>{example.source}</ExampleSource>
+                                        <ExampleSource
+                                            dangerouslySetInnerHTML={{
+                                                __html: DOMPurify.sanitize(example.source, {
+                                                    ALLOWED_TAGS: ["b"],
+                                                }),
+                                            }}
+                                        />
                                     )}
-                                    {example.target && <div>{example.target}</div>}
+                                    {example.target && (
+                                        <div
+                                            // eslint-disable-next-line react/no-danger
+                                            dangerouslySetInnerHTML={{
+                                                __html: DOMPurify.sanitize(example.target, {
+                                                    ALLOWED_TAGS: ["b"],
+                                                }),
+                                            }}
+                                        />
+                                    )}
                                 </ExampleItem>
                             ))}
                         </ExampleList>
@@ -250,9 +266,11 @@ export const Block = styled.div`
 
 const Source = styled(Block)`
     font-weight: normal;
+    white-space: pre-wrap;
 `;
 const Target = styled(Block)`
     font-weight: normal;
+    white-space: pre-wrap;
 `;
 const Detail = styled(Block)`
     font-weight: normal;
@@ -501,4 +519,13 @@ function copyContent(_, action) {
         document.execCommand("copy");
     } else if (!action.copy) window.getSelection().removeAllRanges();
     return action.copy;
+}
+
+/**
+ * Format the result text.
+ * 1. Delete duplicate break line characters
+ */
+function format(text) {
+    if (typeof text !== "string") return text;
+    return text.replace(/\n+/g, "\n");
 }

src/content/display/library/render.js
@@ -1,25 +1,0 @@
-export default format;
-
-/**
- * This function does two operations:
- *
- * 1. Escape HTML tag to avoid XSS security problems.
- * 2. Replace line breakers with <br/> tag so that line break can be displayed.
- *
- * @param {String} text text to format
- *
- * @returns {String} formatted text
- */
-function format(text) {
-    const HTML_CHAR_REGEX = /"|&|'|<|>/g;
-    const LINE_BREAKERS_REGEX = /\r|\n/g;
-
-    if (typeof text !== "string") return text;
-    return text
-        .replace(HTML_CHAR_REGEX, (expression) => {
-            let char = expression.charCodeAt(0);
-            char = char == 0x20 ? 0xa0 : char;
-            return `&#${char};`;
-        })
-        .replace(LINE_BREAKERS_REGEX, "<br/>");
-}

commit 0c4b7b2bf7a97327b27cf73e59913f96fde580d5

fix: inaccurate position of floating panel
This is caused by the commit(1211ae83cbcac031fc83be81c0b268b397068e1c).

src/content/display/Panel.jsx
@@ -142,6 +142,7 @@ export default function ResultPanel() {
                 window.translateResult.originalText = detail.text;
                 setOpen(true);
                 setContentType("LOADING");
+                setContent(detail);
             }
         });
 
@@ -408,6 +409,7 @@ export default function ResultPanel() {
                     0,
                 ];
             showFloatingPanel();
+            console.log(contentRef.current.position, { x: position[0], y: position[1] });
             moveablePanelRef.current.request("draggable", { x: position[0], y: position[1] });
         } else {
             showFixedPanel();