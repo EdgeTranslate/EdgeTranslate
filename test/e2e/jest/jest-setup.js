d0d6050e9c46f481891f70bb7f108a3878b98c06
Nicky Chen <chenjinlong2016@outlook.com>
2021-06-10 17:40:58 +0800

    test: fix a snapshot match problem
    (cherry picked from commit 85301bcd3c4b0e6702664f5f029632f8b7e8a557)

--- test/e2e/jest/jest-setup.js
+++ test/e2e/jest/jest-setup.js
@@ -4,5 +4,7 @@ const path = require("path");
 const toMatchImageSnapshot = configureToMatchImageSnapshot({
     customSnapshotsDir: path.resolve(__dirname, "../image_snapshots"),
     comparisonMethod: "ssim",
+    failureThreshold: 0.1,
+    failureThresholdType: "percent",
 });
 expect.extend({ toMatchImageSnapshot });
