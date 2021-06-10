688e7c5248dc57017206391ff8b1e0b40e3925eb
Yun Feng <yun.feng@smartx.com>
2021-06-10 23:44:37 +0800

    test: divide snapshot images by OS and browser types
    (cherry picked from commit 245ac81f7af0a8b7448c1de62ee3de76ec8e8149)

--- test/e2e/jest/jest-setup.js
+++ test/e2e/jest/jest-setup.js
@@ -2,7 +2,12 @@ const { configureToMatchImageSnapshot } = require("jest-image-snapshot");
 const path = require("path");
 
 const toMatchImageSnapshot = configureToMatchImageSnapshot({
-    customSnapshotsDir: path.resolve(__dirname, "../image_snapshots"),
+    customSnapshotsDir: path.resolve(
+        __dirname,
+        "../image_snapshots",
+        process.env.SELENIUM_BROWSER,
+        process.platform
+    ),
     comparisonMethod: "ssim",
     failureThreshold: 0.1,
     failureThresholdType: "percent",
