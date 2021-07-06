5baf3fb3a10b6501ff2801b1200086ef56b25972
gdh1995 <gdh1995@qq.com>
2021-07-06 10:53:11 +0800

    style: injector for vimium-c

--- static/pdf/vimium-c-injector.js
+++ static/pdf/vimium-c-injector.js
@@ -1,12 +1,13 @@
-"use strict";
+"use strict"; // eslint-disable-line strict
 (function () {
     /**
      * Vimium C 生命周期回调函数，在 https://github.com/gdh1995/vimium-c/blob/master/content/injected_end.ts 中调用
-     * 
+     *
      * @argument {number} action_code 1: "initing", 2: "complete", 3: "destroy"
      */
-    const VimiumCHandler = (action_code, _action_name) => {
-        if (action_code === 2) { // 初始化完成
+    const VimiumCHandler = (action_code) => {
+        if (action_code === 2) {
+            // 初始化完成
             const api = window.VApi;
             const oldScroll = api.$;
             if (typeof oldScroll === "function") {
@@ -14,13 +15,18 @@
                  * 接管滚动命令，用于全屏模式下立即翻页（忽略平滑滚动）
                  */
                 api.$ = (element, di, amount) => {
-                    if (element.id === "viewerContainer" && element.classList.contains("pdfPresentationMode")) {
-                        element.dispatchEvent(new WheelEvent("wheel", {
-                            bubbles: true,
-                            cancelable: true,
-                            composed: true,
-                            deltaY: Math.sign(amount) * 120
-                        }));
+                    if (
+                        element.id === "viewerContainer" &&
+                        element.classList.contains("pdfPresentationMode")
+                    ) {
+                        element.dispatchEvent(
+                            new WheelEvent("wheel", {
+                                bubbles: true,
+                                cancelable: true,
+                                composed: true,
+                                deltaY: Math.sign(amount) * 120,
+                            })
+                        );
                     } else {
                         oldScroll.call(this, element, di, amount);
                     }
@@ -33,23 +39,26 @@
                 const file = new URLSearchParams(location.search).get("file");
                 return file || location.href;
             };
-        } else if (action_code === 3) { // 析构
+        } else if (action_code === 3) {
+            // 析构
             window.removeEventListener("vimiumMark", onMark, true);
         }
     };
 
     /**
      * 设置或者获取“文档滚动位置”，在 https://github.com/gdh1995/vimium-c/blob/master/content/marks.ts#L10 中调用
+     *
+     * @argument {CustomEvent} event
      */
     const onMark = (event) => {
-        const channelElement = event.relatedTarget
+        const channelElement = event.relatedTarget;
         const box = channelElement && document.getElementById("viewerContainer");
         if (!box) {
             return;
         }
-        box.classList.contains("pdfPresentationMode")
-        const str = channelElement.textContent
-        if (str) { // Marks.activate
+        const str = channelElement.textContent;
+        if (str) {
+            // 对应命令 Marks.activate
             const mark = str.split(",");
             const position = [~~mark[0], ~~mark[1]];
             if (position[0] > 0 || position[1] > 0) {
@@ -57,23 +66,25 @@
                 channelElement.textContent = "";
                 event.preventDefault();
             }
-        } else { // Marks.activateCreate
+        } else {
+            // 对应命令 Marks.activateCreate
             channelElement.textContent = [box.scrollLeft, box.scrollTop];
         }
     };
 
     chrome.storage.sync.get("vimiumExtensionInjector", (result) => {
-        let injectorURL = result.vimiumExtensionInjector
+        let injectorURL = result.vimiumExtensionInjector;
         if (injectorURL === "nul" || injectorURL === "/dev/null") {
             return;
         }
         if (!injectorURL) {
-            injectorURL = /\sEdg\//.test(navigator.appVersion) ? "aibcglbfblnogfjhbcmmpobjhnomhcdo"
+            injectorURL = /\sEdg\//.test(navigator.appVersion)
+                ? "aibcglbfblnogfjhbcmmpobjhnomhcdo"
                 : "hfjbmagddngcpeloejdejnfgbamkjaeg";
             chrome.storage.sync.set({ vimiumExtensionInjector: injectorURL });
         }
         if (!injectorURL.includes("://")) {
-            injectorURL = "chrome-extension://" + injectorURL + "/lib/injector.js";
+            injectorURL = `chrome-extension://${injectorURL}/lib/injector.js`;
         }
         const script = document.createElement("script");
         script.src = injectorURL;
@@ -82,8 +93,10 @@
             // 在 https://github.com/gdh1995/vimium-c/blob/master/lib/injector.ts#L87 处定义
             const injector = window.VimiumInjector;
             if (injector) {
-                injector.cache ? VimiumCHandler(2, "") : injector.callback = VimiumCHandler;
-                window.addEventListener("vimiumMark", onMark, true)
+                injector.cache
+                    ? VimiumCHandler(2, "complete")
+                    : (injector.callback = VimiumCHandler);
+                window.addEventListener("vimiumMark", onMark, true);
             }
         };
         document.head.appendChild(script);
