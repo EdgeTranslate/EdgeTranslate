5ce01cc25688e6594ccf6a3ca253861c59761912
Mark-fenng <f18846188605@gmail.com>
2021-07-14 16:55:19 +0800

    fix: injection of display.js error
    Uncaught Error: Automatic publicPath is not supported in this browser

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -13,6 +13,7 @@ module.exports = {
     output: {
         filename: "[name].js",
         path: path.resolve(__dirname, "../build"),
+        publicPath: "./",
     },
     module: {
         rules: [
