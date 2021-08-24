f5269832651462b4a304123ba1ffa4aa6fe6c894
Nicky Chen <chenjinlong2016@outlook.com>
2021-08-24 21:45:24 +0800

    fix: error when opening PDF files on www.yuque.com (#261)

--- src/content/pdf.js
+++ src/content/pdf.js
@@ -14,16 +14,29 @@ const channel = new Channel();
 window.addEventListener("load", () => {
     if (isChromePDFViewer()) {
         let state = history.state;
+
+        /**
+         * Issue: https://github.com/EdgeTranslate/EdgeTranslate/issues/261
+         *
+         * Some websites imitate the structure of Chrome built-in PDF viewer page
+         * but use a separated pdf source instead of window.location.href, like
+         * https://www.yuque.com/.
+         */
+        let pdfSrc = window.location.href;
+        if (document.body.children[0].src && document.body.children[0].src !== "about:blank") {
+            pdfSrc = document.body.children[0].src;
+        }
+
         if (state === null) {
             // 第一次打开页面，直接跳转到PDF.js阅读器，并将ET_visited设为真
             state = { ET_visited: true };
             history.replaceState(state, document.title, window.location.href);
-            redirect();
+            redirect(pdfSrc);
         } else if (!state.ET_visited) {
             // 没设置过ET_visited，或者ET_visited为假，需要跳转到PDF.js阅读器，并将ET_visited设为真
             state.ET_visited = true;
             history.replaceState(state, document.title, window.location.href);
-            redirect();
+            redirect(pdfSrc);
         } else {
             // ET_visited为真，说明是从PDF.js阅读器返回，不再跳转到PDF.js阅读器，并将ET_visited设为假
             state.ET_visited = false;
@@ -34,15 +47,15 @@ window.addEventListener("load", () => {
 
 /**
  * 向background.js发送消息实现跳转。
+ *
+ * @param {String} pdfSrc PDF file source.
  */
-function redirect() {
+function redirect(pdfSrc) {
     chrome.storage.sync.get("OtherSettings", (result) => {
         let OtherSettings = result.OtherSettings;
         if (OtherSettings && OtherSettings["UsePDFjs"]) {
             channel.emit("redirect", {
-                url: chrome.runtime.getURL(
-                    `pdf/viewer.html?file=${encodeURIComponent(window.location.href)}`
-                ),
+                url: chrome.runtime.getURL(`pdf/viewer.html?file=${encodeURIComponent(pdfSrc)}`),
             });
         }
     });
