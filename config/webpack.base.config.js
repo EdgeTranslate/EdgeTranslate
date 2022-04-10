39ab59830f987c28e7ce48ab1ab98704a35ebc27
Nicky Chen <chenjinlong2016@outlook.com>
2022-04-10 16:44:56 +0800

    style: use '_' instead of '-' in file names

--- config/webpack.base.config.js
+++ config/webpack.base.config.js
@@ -4,13 +4,13 @@ module.exports = {
     entry: {
         "/background/background": "./src/background/background.js",
         "/content/pdf": "./src/content/pdf.js",
-        "/content/banner-controller": "./src/content/banner-controller.js",
+        "/content/banner_controller": "./src/content/banner_controller.js",
         "/content/select/select": "./src/content/select/select.js",
         "/content/display/display": "./src/content/display/index.js",
         "/content/notice/notice": "./src/content/notice/notice.js",
         "/popup/popup": "./src/popup/popup.js",
         "/options/options": "./src/options/options.js",
-        "/content/deepl-injector": "./src/content/deepl-injector.js",
+        "/content/deepl_injector": "./src/content/deepl_injector.js",
     },
     output: {
         filename: "[name].js",
